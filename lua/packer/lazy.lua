71110d5e618f4a5521cc0d6b3df780bfd9f9e868
nanpangyou <yigehd@foxmail.com>
2023-07-31 16:32:10 +0800

    init

---
+++ lua/packer/lazy.lua
@@ -0,0 +1,16 @@
+-- init lazy.nvim  安装 lazy.nvim 包管理器
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- 添加插件
+require("lazy").setup("plugins")
